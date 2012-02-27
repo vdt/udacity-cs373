29c55c33808275f14e906270aa56fd41335b9094
daniel watson <ozzloy@gmail.com>
2012-02-26 19:57:07 -0500

    add hw-1.4 localization program

--- unit-1.py
+++ unit-1.py
@@ -1,38 +1,36 @@
 #! /usr/bin/env python
 
-#Modify the previous code so that the robot senses red twice.
+colors = [['red', 'green', 'green', 'red' , 'red'],
+          ['red', 'red', 'green', 'red', 'red'],
+          ['red', 'red', 'green', 'green', 'red'],
+          ['red', 'red', 'red', 'red', 'red']]
 
-p=[0.2, 0.2, 0.2, 0.2, 0.2]
-world=['green', 'red', 'red', 'green', 'green']
-measurements = ['red', 'red']
-motions = [1,1]
-pHit = 0.6
-pMiss = 0.2
-pExact = 0.8
-pOvershoot = 0.1
-pUndershoot = 0.1
+measurements = ['green', 'green', 'green' ,'green', 'green']
 
-def sense(p, Z):
-    q=[]
+
+motions = [[0,0],[0,1],[1,0],[1,0],[0,1]]
+
+sensor_right = 0.7
+
+p_move = 0.8
+
+def show(p):
     for i in range(len(p)):
-        hit = (Z == world[i])
-        q.append(p[i] * (hit * pHit + (1-hit) * pMiss))
-    s = sum(q)
-    for i in range(len(q)):
-        q[i] = q[i] / s
-    return q
+        print p[i]
 
-def move(p, U):
-    q = []
-    for i in range(len(p)):
-        s = pExact * p[(i-U) % len(p)]
-        s = s + pOvershoot * p[(i-U-1) % len(p)]
-        s = s + pUndershoot * p[(i-U+1) % len(p)]
-        q.append(s)
-    return q
+#DO NOT USE IMPORT
+#ENTER CODE BELOW HERE
+#ANY CODE ABOVE WILL CAUSE
+#HOMEWORK TO BE GRADED
+#INCORRECT
 
-for k in range(len(measurements)):
-    p = sense(p, measurements[k])
-    p = move(p, motions[k])
+p = []
 
-print p         
+
+
+
+
+#Your probability array must be printed 
+#with the following code.
+
+show(p)
