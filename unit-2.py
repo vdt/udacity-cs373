83662989b132b8d3bb6505e7841f1be19b04f4ea
daniel watson <ozzloy@gmail.com>
2012-03-01 22:01:42 -0500

    add unit-2.3 bonus question: coconuts 2 in python

---
+++ unit-2.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+#Write a function that takes the number of 
+#coconuts, n, as an argument and returns the
+#number of coconuts after one is given to
+#the monkey and one fifth are taken away.
+
+
+def f(n):
+    #
+    # Insert code here
+    #
+    return
+
+print f(96.)
