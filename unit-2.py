a4110147c65ee323e474088a2691fe4f6a0fbaeb
daniel watson <ozzloy@gmail.com>
2012-03-04 12:51:40 -0500

    add unit-2.24 predict function

--- unit-2.py
+++ unit-2.py
@@ -1,20 +1,18 @@
 #! /usr/bin/env python
 
-# Write a program to update your mean and variance
-# when given the mean and variance of your belief
-# and the mean and variance of your measurement.
-# This program will update the parameters of your
-# belief function.
-
-def sigma_prime(mu, sigma_square, nu, r_square):
-    return (r_square * mu + sigma_square * nu)/(sigma_square + r_square)
-
-def sigma_square_prime(sigma_square, r_square):
-    return 1./(1./sigma_square + 1./r_square)
+# Write a program that will predict your new mean
+# and variance given the mean and variance of your 
+# prior belief and the mean and variance of your 
+# motion. 
 
 def update(mean1, var1, mean2, var2):
-    new_mean = sigma_prime(mean1, var1, mean2, var2)
-    new_var = sigma_square_prime(var1, var2)
+    new_mean = (var2 * mean1 + var1 * mean2) / (var1 + var2)
+    new_var = 1/(1/var1 + 1/var2)
     return [new_mean, new_var]
 
-print update(10.,8.,13., 2.)
+def predict(mean1, var1, mean2, var2):
+    new_mean =
+    new_var =
+    return [new_mean, new_var]
+
+print predict(10., 4., 12., 4.)
