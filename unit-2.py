e63296c66373f9d2e91f5468774a4edcfa024661
daniel watson <ozzloy@gmail.com>
2012-03-04 12:52:48 -0500

    add unit-2.24 predict function solution

--- unit-2.py
+++ unit-2.py
@@ -11,8 +11,8 @@ def update(mean1, var1, mean2, var2):
     return [new_mean, new_var]
 
 def predict(mean1, var1, mean2, var2):
-    new_mean =
-    new_var =
+    new_mean = mean1 + mean2
+    new_var = var1 + var2
     return [new_mean, new_var]
 
 print predict(10., 4., 12., 4.)
