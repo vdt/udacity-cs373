5fb2d2b770cae80b03a439e423207db97152eee4
daniel watson <ozzloy@gmail.com>
2012-03-05 00:00:10 -0500

    add homework 2 question 6 final answer

--- hw2-6.py
+++ hw2-6.py
@@ -170,11 +170,11 @@ print "### 4-dimensional example ###"
 measurements = [[5., 10.], [6., 8.], [7., 6.], [8., 4.], [9., 2.], [10., 0.]]
 initial_xy = [4., 12.]
 
-# measurements = [[1., 4.], [6., 0.], [11., -4.], [16., -8.]]
-# initial_xy = [-4., 8.]
+measurements = [[1., 4.], [6., 0.], [11., -4.], [16., -8.]]
+initial_xy = [-4., 8.]
 
-# measurements = [[1., 17.], [1., 15.], [1., 13.], [1., 11.]]
-# initial_xy = [1., 19.]
+measurements = [[1., 17.], [1., 15.], [1., 13.], [1., 11.]]
+initial_xy = [1., 19.]
 
 dt = 0.1
 
@@ -185,16 +185,21 @@ u = matrix([[0.], [0.], [0.], [0.]]) # external motion
 #### fill this in, remember to use the matrix() function!: ####
 
 # initial uncertainty
-P =
+P = matrix([[   0.,    0.,    0.,    0.],
+            [   0.,    0.,    0.,    0.],
+            [   0.,    0., 1000.,    0.],
+            [   0.,    0.,    0., 1000.]])
 # next state function
-F = matrix([[1., 0., 1., 0.],
-            [0., 1., 0., 1.],
+F = matrix([[1., 0., dt, 0.],
+            [0., 1., 0., dt],
             [0., 0., 1., 0.],
             [0., 0., 0., 1.]])
 # measurement function
-H =
+H = matrix([[1., 0., 0., 0.],
+            [0., 1., 0., 0.]])
 # measurement uncertainty
-R =
+R = matrix([[0.1,  0.],
+            [ 0., 0.1]])
 # identity matrix
 I = matrix([[1., 0., 0., 0.],
             [0., 1., 0., 0.],
