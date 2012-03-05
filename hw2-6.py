53694fb4583c8bb44f5be0befb45159916bdb74b
daniel watson <ozzloy@gmail.com>
2012-03-04 23:14:49 -0500

    add homework 2 question 6 initial code

--- hw2-6.py
+++ hw2-6.py
@@ -1,7 +1,10 @@
 #! /usr/bin/env python
 
-# Write a function 'filter' that implements a multi-
-# dimensional Kalman Filter for the example given
+# Fill in the matrices P, F, H, R and I at the bottom
+#
+# This question requires NO CODING, just fill in the 
+# matrices where indicated. Please do not delete or modify
+# any provided code OR comments. Good luck!
 
 from math import *
 
@@ -90,8 +93,7 @@ class matrix:
                 res.value[j][i] = self.value[i][j]
         return res
     
-    # Thanks to Ernesto P. Adorio for use of Cholesky and
-    #  CholeskyInverse functions
+    # Thanks to Ernesto P. Adorio for use of Cholesky and CholeskyInverse functions
     
     def Cholesky(self, ztol=1.0e-5):
         # Computes the upper triangular Cholesky factorization of
@@ -141,50 +143,53 @@ class matrix:
 
 ########################################
 
-# Implement the filter function below
-def measure(measurement, x, P, H, R):
-    # error calculation
-    y = measurement - H * x
-    S = H * P * H.transpose() + R
-    # kalman gain:
-    K = P * H.transpose() * S.inverse()
-    # measurement step:
-    x_prime = x + (K * y)
-    P_prime = (I - K * H) * P
-    return [x_prime, P_prime]
-
-def predict(x, P, F, u):
-    x_prime = F * x + u
-    P_prime = F * P * F.transpose()
-    return [x_prime, P_prime]
-
 def filter(x, P):
     for n in range(len(measurements)):
-        measurement = matrix([[float(measurements[n])]])
-        
-        # measurement update
-        x, P = measure(measurement, x, P, H, R)
         
         # prediction
-        x, P = predict(x, P, F, u)
-
-        print 'x= '
-        x.show()
-        print 'P= '
-        P.show()
-
-
+        x = (F * x) + u
+        P = F * P * F.transpose()
+        
+        # measurement update
+        Z = matrix([measurements[n]])
+        y = Z.transpose() - (H * x)
+        S = H * P * H.transpose() + R
+        K = P * H.transpose() * S.inverse()
+        x = x + (K * y)
+        P = (I - (K * H)) * P
+    
+    print 'x= '
+    x.show()
+    print 'P= '
+    P.show()
 
 ########################################
 
-measurements = [1, 2, 3]
+print "### 4-dimensional example ###"
 
-x = matrix([[0.], [0.]]) # initial state (location and velocity)
-P = matrix([[1000., 0.], [0., 1000.]]) # initial uncertainty
-u = matrix([[0.], [0.]]) # external motion
-F = matrix([[1., 1.], [0, 1.]]) # next state function
-H = matrix([[1., 0.]]) # measurement function
-R = matrix([[1.]]) # measurement uncertainty
-I = matrix([[1., 0.], [0., 1.]]) # identity matrix
+measurements = [[5., 10.], [6., 8.], [7., 6.], [8., 4.], [9., 2.], [10., 0.]]
+initial_xy = [4., 12.]
+
+# measurements = [[1., 4.], [6., 0.], [11., -4.], [16., -8.]]
+# initial_xy = [-4., 8.]
+
+# measurements = [[1., 17.], [1., 15.], [1., 13.], [1., 11.]]
+# initial_xy = [1., 19.]
+
+dt = 0.1
+
+x = matrix([[initial_xy[0]], [initial_xy[1]], [0.], [0.]]) # initial state (location and velocity)
+u = matrix([[0.], [0.], [0.], [0.]]) # external motion
+
+#### DO NOT MODIFY ANYTHING ABOVE HERE ####
+#### fill this in, remember to use the matrix() function!: ####
+
+P =  # initial uncertainty
+F =  # next state function
+H =  # measurement function
+R =  # measurement uncertainty
+I =  # identity matrix
+
+###### DO NOT MODIFY ANYTHING HERE #######
 
 filter(x, P)
