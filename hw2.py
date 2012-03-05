dc22cd480a7dfa2b45e2592922f07e72992c0d1b
daniel watson <ozzloy@gmail.com>
2012-03-04 22:39:16 -0500

    add homework 2 with question 1

---
+++ hw2.py
@@ -0,0 +1,6 @@
+#! /usr/bin/env python
+
+# question 1
+print "is sigma-square of gaussian from combining 2 other gaussians"
+print "of same variance smaller, larger, same as old variance?"
+print "smaller variance, higher information content"
