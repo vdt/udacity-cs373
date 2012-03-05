a942427bc20d83006e39ff936999837888919c1b
daniel watson <ozzloy@gmail.com>
2012-03-04 22:51:32 -0500

    add homework 2 question 4, how many dimensions

--- hw2.py
+++ hw2.py
@@ -1,17 +1,26 @@
 #! /usr/bin/env python
 
 # question 1
+print "measurement update"
 print "is sigma-square of gaussian from combining 2 other gaussians"
 print "of same variance smaller, larger, same as old variance?"
 print "smaller variance, higher information content"
 
 # question 2
+print "new variance"
 print "say we have a prior gaussian, mu, sigma-square"
 print "and a measurement gaussian with same parameters."
 print "what will covariance of new gaussian be after multiplying these?"
 print "new sigma-square will be half of old sigma-square"
 
 # question 3
+print "heavytail gaussian"
 print "is it possible to have a heavytail gaussian using"
 print " the standard gaussian formula and changing mu and sigma?"
 print "no"
+
+# question 4
+print "how many dimensions?"
+print "object tracked in 2D using kalman filters estimating"
+print "position and velocity"
+print "how many dimensions in the state vector?"
