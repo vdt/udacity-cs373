618c0c5f8825b45697a80f693f2d802654985541
daniel watson <ozzloy@gmail.com>
2012-03-04 22:41:58 -0500

    add homework 2 question 2

--- hw2.py
+++ hw2.py
@@ -4,3 +4,9 @@
 print "is sigma-square of gaussian from combining 2 other gaussians"
 print "of same variance smaller, larger, same as old variance?"
 print "smaller variance, higher information content"
+
+# question 2
+print "say we have a prior gaussian, mu, sigma-square"
+print "and a measurement gaussian with same parameters."
+print "what will covariance of new gaussian be after multiplying these?"
+print "new sigma-square will be half of old sigma-square"
