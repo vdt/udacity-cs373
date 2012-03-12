58675361b4c9653647d3b90bf95a25531d79731e
daniel watson <ozzloy@gmail.com>
2012-03-11 23:26:05 -0400

    add hw3.1 empty cell solution

--- hw3.py
+++ hw3.py
@@ -4,3 +4,6 @@
 print "what is the probability that zero particles are in stat A?"
 print "4 states, N uniform particles"
 print "when N = 1, 4, 10?"
+def pN(N):
+    return 3./4.**N
+print pN(1), pN(4), pN(10)
