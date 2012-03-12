e5c7737056d301df8311824593ffe392bc49ea4e
daniel watson <ozzloy@gmail.com>
2012-03-11 20:53:21 -0400

    add unit-3.9 moving robot solution

--- unit-3.py
+++ unit-3.py
@@ -118,3 +118,8 @@ def eval(r, p):
 ####   DON'T MODIFY ANYTHING ABOVE HERE! ENTER CODE BELOW ####
 
 myrobot = robot()
+myrobot.set(30., 50., pi/2.)
+myrobot = myrobot.move(-pi/2., 15)
+print myrobot.sense()
+myrobot = myrobot.move(-pi/2., 10)
+print myrobot.sense()
