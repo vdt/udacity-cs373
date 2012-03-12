62ebee45218d2e657cba1b8f5a096f0b809d58bb
daniel watson <ozzloy@gmail.com>
2012-03-11 23:30:00 -0400

    add hw3.2 motion question

--- hw3.py
+++ hw3.py
@@ -7,3 +7,11 @@ print "when N = 1, 4, 10?"
 def pN(N):
     return 3./4.**N
 print pN(1), pN(4), pN(10)
+
+# 2. motion question
+print "consider 4 states, [[a b],[c d]].  motion step"
+print "50% = p(move horizontally), 50% = p(vertically), 0% diagonal"
+print "never stays in same cell"
+print "after 1 step, how many do we expect in each cell,"
+print "given we start with [[5 3] [3 1]]?"
+print "also what do we expect after infinite steps?"
