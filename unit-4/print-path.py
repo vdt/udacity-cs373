6a13a9781b496cc3df921946d8d83af253c77152
daniel watson <ozzloy@gmail.com>
2012-03-19 01:02:16 -0400

    add unit-4/print-path solution

--- unit-4/print-path.py
+++ unit-4/print-path.py
@@ -5,15 +5,15 @@
 #
 # Modify the the search function so that it returns
 # a shortest path as follows:
-# 
+#
 # [['>', 'v', ' ', ' ', ' ', ' '],
 #  [' ', '>', '>', '>', '>', 'v'],
 #  [' ', ' ', ' ', ' ', ' ', 'v'],
 #  [' ', ' ', ' ', ' ', ' ', 'v'],
 #  [' ', ' ', ' ', ' ', ' ', '*']]
 #
-# Where '>', '<', '^', and 'v' refer to right, left, 
-# up, and down motions. NOTE: the 'v' should be 
+# Where '>', '<', '^', and 'v' refer to right, left,
+# up, and down motions. NOTE: the 'v' should be
 # lowercase.
 #
 # Your function should be able to do this for any
@@ -23,10 +23,10 @@
 
 # Sample Test case
 grid = [[0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0],
         [0, 0, 1, 0, 1, 0],
         [0, 0, 1, 0, 1, 0],
-        [0, 0, 1, 0, 1, 0]]
+        [0, 0, 0, 0, 1, 0]]
 
 init = [0, 0]
 goal = [len(grid)-1, len(grid[0])-1]
@@ -44,45 +44,45 @@ cost = 1
 # modify code below
 # ----------------------------------------
 
-def search():
-    closed = [[0 for row in range(len(grid[0]))] for col in range(len(grid))]
-    closed[init[0]][init[1]] = 1
+def neighbors(i, j):
+    bros = []
+    for h in range(len(delta)):
+        di, dj = delta[h]
+        bro = [delta_name[h], [i + di, j + dj]]
+        bros.append(bro)
+    return bros
 
-    x = init[0]
-    y = init[1]
+def in_grid(i, j):
+    return i in range(len(grid)) and j in range(len(grid[0]))
+
+def valid_neighbors(i, j, closed):
+    bros = []
+    for name, [p, q] in neighbors(i, j):
+        if in_grid(p, q) and not closed[p][q]:
+            bros.append([name, [p, q]])
+    return bros
+
+def search():
+    map = [[' ' for cell in row] for row in grid]
+    map[goal[0]][goal[1]] = '*'
+    closed = [[False if cell == 0 else True for cell in row] for row in grid]
+    closed[init[0]][init[1]] = True
     g = 0
 
-    open = [[g, x, y]]
+    trail = []
+    open = [[g, init, trail]]
 
-    found = False  # flag that is set when search is complet
-    resign = False # flag set if we can't find expand
-
-    while not found and not resign:
-        if len(open) == 0:
-            resign = True
-            return 'fail'
-        else:
-            open.sort()
-            open.reverse()
-            next = open.pop()
-            x = next[1]
-            y = next[2]
-            g = next[0]
-            
-            if x == goal[0] and y == goal[1]:
-                found = True
-            else:
-                for i in range(len(delta)):
-                    x2 = x + delta[i][0]
-                    y2 = y + delta[i][1]
-                    if x2 >= 0 and x2 < len(grid) and y2 >=0 and y2 < len(grid[0]):
-                        if closed[x2][y2] == 0 and grid[x2][y2] == 0:
-                            g2 = g + cost
-                            open.append([g2, x2, y2])
-                            closed[x2][y2] = 1
-    for i in range(len(expand)):
-        print expand[i]
-    return # make sure you return the shortest path.
-
-
-
+    while open:
+        open.sort()
+        open.reverse()
+        g, [i, j], trail = open.pop()
+        if [i, j] == goal:
+            break
+        g += cost
+        for name, [p, q] in valid_neighbors(i, j, closed):
+            new_trail = trail + [[name, [i, j]]]
+            open.append([g, [p, q], new_trail])
+            closed[p][q] = True
+    for name, [i, j] in trail:
+        map[i][j] = name
+    return map # make sure you return the shortest path.
