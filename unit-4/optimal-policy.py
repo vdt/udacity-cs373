0ff170be8441483e3ac43915c7255c155ccfe24a
daniel watson <ozzloy@gmail.com>
2012-03-19 14:02:03 -0400

    add unit-4/optimal-policy solution

--- unit-4/optimal-policy.py
+++ unit-4/optimal-policy.py
@@ -2,22 +2,23 @@
 
 # ----------
 # User Instructions:
-# 
+#
 # Create a function optimum_policy() that returns
 # a grid which shows the optimum policy for robot
 # motion. This means there should be an optimum
 # direction associated with each navigable cell.
-# 
+#
 # un-navigable cells must contain an empty string
 # WITH a space, as shown in the previous video.
 # Don't forget to mark the goal with a '*'
 
 # ----------
 
-grid = [[0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
+grid = [[0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0],
+        [0, 0, 1, 1, 1, 0],
         [0, 0, 0, 0, 1, 0]]
 
 init = [0, 0]
@@ -35,34 +36,72 @@ cost_step = 1 # the cost associated with moving from a cell to an adjacent one.
 # ----------------------------------------
 # modify code below
 # ----------------------------------------
+def neighbors(i, j):
+    return [[i + di, j + dj] for di, dj in delta]
+
+def in_grid(i, j):
+    return i in range(len(grid)) and j in range(len(grid[0]))
+
+def valid_cell(i, j, closed):
+    return in_grid(i, j) and not closed[i][j]
+
+def valid_neighbors(i, j, closed):
+    return [[p, q] for p, q in neighbors(i, j) if valid_cell(p, q, closed)]
+
+def recursive_value_grid(open, closed, value_grid):
+    open.sort()
+    if open:
+        value, [i, j] = open[0]
+        value_grid[i][j] = value
+        value_prime = value + cost_step
+        for p, q in valid_neighbors(i, j, closed):
+            closed[p][q] = True
+            open.append([value_prime, [p, q]])
+        return recursive_value_grid(open[1:], closed, value_grid)
+    else:
+        return value_grid
+
+def compute_value():
+    closed = [[False if cell == 0 else True for cell in row] for row in grid]
+    closed[goal[0]][goal[1]] = True
+    value_grid = [[99 for cell in row] for row in grid]
+    value = 0
+    open = [[value, goal]]
+
+    return recursive_value_grid(open, closed, value_grid)
+
+def named_neighbors(i, j):
+    bros = []
+    for h in range(len(delta)):
+        di, dj = delta[h]
+        name = delta_name[h]
+        bros.append([[i + di, j + dj], name])
+    return bros
+
+def valid_neighbors_name_value(i, j, closed, value):
+    bros = []
+    for [p, q], name in named_neighbors(i, j):
+        if valid_cell(p, q, closed):
+            bros.append([name, value[p][q]])
+    return bros
+
+def minimum_neighbor_name(i, j, closed, value):
+    bros = []
+    minimum_name = ' '
+    minimum = 99
+    for name, value in valid_neighbors_name_value(i, j, closed, value):
+        if value < minimum:
+            minimum = value
+            minimum_name = name
+    return minimum_name
 
 def optimum_policy():
-    value = [[99 for row in range(len(grid[0]))] for col in range(len(grid))]
-    change = True
-
-    while change:
-        change = False
-
-        for x in range(len(grid)):
-            for y in range(len(grid[0])):
-                if goal[0] == x and goal[1] == y:
-                    if value[x][y] > 0:
-                        value[x][y] = 0
-
-                        change = True
-
-                elif grid[x][y] == 0:
-                    for a in range(len(delta)):
-                        x2 = x + delta[a][0]
-                        y2 = y + delta[a][1]
-
-                        if x2 >= 0 and x2 < len(grid) and y2 >= 0 and y2 < len(grid[0]) and grid[x2][y2] == 0:
-                            v2 = value[x2][y2] + cost_step
-
-                            if v2 < value[x][y]:
-                                change = True
-                                value[x][y] = v2
-
-    return policy # Make sure your function returns the expected grid.
-
-
+    value = compute_value()
+    policy = [[' ' for cell in row] for row in grid]
+    closed = [[False if cell == 0 else True for cell in row] for row in grid]
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            if valid_cell(i, j, closed):
+                policy[i][j] = minimum_neighbor_name(i, j, closed, value)
+    policy[goal[0]][goal[1]] = '*'
+    return policy
